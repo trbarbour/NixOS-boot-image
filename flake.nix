14a16c08327c5289e4c712fd58574d2eb25ac00c
Tim Barbour <trb@categorical.net>
2025-10-26 10:12:47 -0400

    update NixOS version in flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Pre-NixOS setup tool";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
